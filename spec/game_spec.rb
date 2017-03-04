e2825184c1c7b249e284104ba4a13c7d372998de
joemaidman <joemaidman@gmail.com>
2017-03-04 22:44:32 +0000

    Added poison, slepp, heal

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -27,7 +27,7 @@ describe Game do
      expect(game.opponent_of(player_1)).to eq player_2
    end
 
-   it 'sets the loser if a player has 0 HP and sets the game to be over' do
+   it 'sets the loser if a player has zero HP and sets the game to be over' do
      allow(player_1).to receive(:health_points) { 0 }
      allow(player_2).to receive(:health_points) { 10 }
      expect(game.loser).to eq player_1
